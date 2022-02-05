69f5e09c2b33e01e70df55501fe7d61a6f3d47fe
Nipun Bahri <bahri.nipun@gmail.com>
2022-02-05 17:30:27 +0100

    src

---
+++ src/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using k8s;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Load from the default kubeconfig on the machine.
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+
+            // Load from a specific file:
+            //var config = KubernetesClientConfiguration.BuildConfigFromConfigFile(Environment.GetEnvironmentVariable("KUBECONFIG"));
+
+            // Load from in-cluster configuration:
+            //var config = KubernetesClientConfiguration.InClusterConfig();
+
+            // Use the config object to create a client.
+            var client = new Kubernetes(config);
+
+            var namespaces = client.ListNamespace();
+            foreach (var ns in namespaces.Items) {
+                Console.WriteLine(ns.Metadata.Name);
+                var list = client.ListNamespacedPod(ns.Metadata.Name);
+                foreach (var item in list.Items)
+                {
+                    Console.WriteLine(item.Metadata.Name);
+                }
+            }
+
+        }
+    }
+}
